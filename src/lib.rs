ee7072fd3690e5335e6e8864b2f22dcdd6caef0e
nakake <ryota.n@nakahome.com>
2024-02-05 23:01:40 +0900

    depth_texture

--- src/lib.rs
+++ src/lib.rs
@@ -261,6 +261,7 @@ struct State {
     diffuse_texture: texture::Texture,
     instances: Vec<Instance>,
     instance_buffer: wgpu::Buffer,
+    depth_texture: texture::Texture,
 }
 
 impl State {
@@ -440,6 +441,7 @@ impl State {
             }
         );
 
+        let depth_texture = texture::Texture::create_depth_texture(&device, &config, "depth_texture");
         
         let clear_color = wgpu::Color::BLACK;
 
@@ -488,7 +490,13 @@ impl State {
                 polygon_mode: wgpu::PolygonMode::Fill, 
                 conservative: false 
             }, 
-            depth_stencil: None, 
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: texture::Texture::DEPTH_FORMAT,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less,
+                stencil: wgpu::StencilState::default(),
+                bias: wgpu::DepthBiasState::default(),
+            }), 
             multisample: wgpu::MultisampleState {
                 count: 1,
                 mask: !0,
@@ -536,6 +544,7 @@ impl State {
             diffuse_texture,
             instances,
             instance_buffer,
+            depth_texture,
         }
     }
 
@@ -550,6 +559,8 @@ impl State {
             self.config.height = new_size.height;
             self.surface.configure(&self.device, &self.config);
         }
+
+        self.depth_texture = texture::Texture::create_depth_texture(&self.device, &self.config, "depth_texture");
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
@@ -576,7 +587,14 @@ impl State {
                         store: wgpu::StoreOp::Store,
                     },
                 })],
-                depth_stencil_attachment: None,
+                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                    view: &self.depth_texture.view,
+                    depth_ops: Some(wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(1.0),
+                        store: wgpu::StoreOp::Store
+                    }),
+                    stencil_ops: None
+                }),
                 occlusion_query_set: None,
                 timestamp_writes: None,
             });
